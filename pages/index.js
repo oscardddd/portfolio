0ae4d6ca8d24136e181de62ecdcb134a6fe854da
oscarddddddd <68830568+oscardddd@users.noreply.github.com>
2022-12-31 10:57:23 -0600

    polish

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styles from "./index.module.css"
 import Head from 'next/head'
-import {AiFillGithub,AiOutlineWechat,AiOutlineMail,AiOutlineLinkedin,AiOutlineInstagram} from "react-icons/ai";
+import {AiFillGithub,AiOutlineWechat,AiOutlineMail,AiOutlineLinkedin,AiOutlineInstagram, AiOutlineHome,AiOutlineExperiment,
+  AiOutlineLaptop,AiOutlineZhihu} from "react-icons/ai";
 import Link from "next/link";
 import Image from "next/image";
 import profilepic from "../public/pic2.jpg"
@@ -9,11 +10,46 @@ export default function Home() {
   return(
   
     <div className={styles.background}>
+      <Head className = {styles.head}>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <div className={styles.banner}>
+          <div className = {styles.text}>Hi! This is Qianli</div>
+        </div>
+        <div>
+          <div className={styles.navbar}>
+            <div className={styles.leftnav}>
+              <Link href= "/" className={styles.r1}>
+                Home
+                <AiOutlineHome/>
+              </Link>
+              <Link href= "/" className={styles.r2}>
+                中文 |
+                <AiOutlineZhihu/>
+              </Link>
+              
+            </div>
+            <div className={styles.rightnav}>
+              <Link href= "/" className={styles.l1}>
+                Projects
+                <AiOutlineExperiment/>
+              </Link>
+              <Link href= "/blog" className={styles.l2}>
+                Blog
+                <AiOutlineLaptop/>
+              </Link>
+              <Link href= "/" className={styles.l3}>
+              Contact
+              <AiOutlineMail/>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Head>
+        
       
-      <div className={styles.head}>
-        <p className = {styles.text}>Hi! This is Qianli</p>
-      </div>
-      <Image src = {profilepic} alt="profile pic" width={500} height={500}/>
+      <Image className={styles.img} src = {profilepic} alt="profile pic" width={300} height={300}/>
+      
+      
       <div className={styles.downnav}>
         <Link href="https://github.com/oscardddd">
           <AiFillGithub style={{ fontSize: '3em',color:'black'}}/>
@@ -34,7 +70,9 @@ export default function Home() {
       <style jsx global>{`
         body {
         margin: none;
-        background-color:rgba(72, 122, 180, .5);
+        background-color:rgba(72, 122, 180, .5)
+        // background-color:white;
+        
       }
     `}</style>
     </div>

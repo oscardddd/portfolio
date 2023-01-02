0457d4399a62f5c0d3cea07b4e41c3ccf0fa4207
oscarddddddd <68830568+oscardddd@users.noreply.github.com>
2023-01-02 01:50:16 -0600

    update

--- pages/index.js
+++ pages/index.js
@@ -8,40 +8,40 @@ import profilepic from "../public/pic2.jpg"
 
 export default function Home() {
   return(
-  
+    <>
+    <div className={styles.head}>
+      <p className = {styles.text}>Hi! This is Qianli</p>
+    </div>
     <div className={styles.background}>
-      
-      <div className={styles.head}>
-        <p className = {styles.text}>Hi! This is Qianli</p>
-      </div>
+
       <div className={styles.navbar}>
-      <div className={styles.leftnav}>
-        <Link href= "/" className={styles.r1}>
-                Home
-                <AiOutlineHome/>
-        </Link>
-        <Link href= "/" className={styles.r2}>
-                中文 |
-                {/* <AiOutlineZhihu/> */}
-        </Link>
+        <div className={styles.leftnav}>
+          <Link href= "/" className={styles.r1}>
+                  Home
+                  <AiOutlineHome/>
+          </Link>
+          <Link href= "/" className={styles.r2}>
+                  中文 |
+                  {/* <AiOutlineZhihu/> */}
+          </Link>
               
-      </div>
-      <div className={styles.rightnav}>
-        <Link href= "/" className={styles.l1}>
-                Projects
-                <AiOutlineExperiment/>
-        </Link>
-        <Link href= "/blog" className={styles.l2}>
-                Blog
-                <AiOutlineLaptop/>
-        </Link>
-        <Link href= "/" className={styles.l3}>
-              Contact
-              <AiOutlineMail/>
-        </Link>
+        </div>
+        <div className={styles.rightnav}>
+          <Link href= "/" className={styles.l1}>
+                  Projects
+                  <AiOutlineExperiment/>
+          </Link>
+          <Link href= "/blog" className={styles.l2}>
+                  Blog
+                  <AiOutlineLaptop/>
+          </Link>
+          <Link href= "/" className={styles.l3}>
+                Contact
+                <AiOutlineMail/>
+          </Link>
         </div>
       </div>
-      <Image src = {profilepic} alt="profile pic" width={500} height={500}/>
+      <Image className= {styles.img} src = {profilepic} alt="profile pic" width={300} height={300}/>
       <div className={styles.downnav}>
         <Link href="https://github.com/oscardddd">
           <AiFillGithub style={{ fontSize: '3em',color:'black'}}/>
@@ -56,17 +56,20 @@ export default function Home() {
         <Link href="https://www.instagram.com/d_qianli/">
           <AiOutlineInstagram style={{fontSize:'3em',color:'black'}}/>
         </Link>
-       
-
-
       </div>
+      </div>
+      
       <style jsx global>{`
         body {
         margin: none;
         background-color:rgba(72, 122, 180, .5);
       }
     `}</style>
-    </div>
+    </>
+    
+
+
+   
    
     
   )

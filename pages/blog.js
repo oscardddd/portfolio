3ad366d63b508e0450e76073664d7a00794eace2
oscarddddddd <68830568+oscardddd@users.noreply.github.com>
2023-01-02 21:17:59 -0600

    about

--- pages/blog.js
+++ pages/blog.js
@@ -4,8 +4,8 @@ import styles from "./blog.module.css"
 export default function Blog(){
     return(
         <div>
-            <Head >
-                <div className={styles.text}>Blog</div>
+            <Head>
+                Blog
             </Head>
             
             

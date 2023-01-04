6c2db88deb434b8825153d58d4e88372201a3f46
oscarddddddd <68830568+oscardddd@users.noreply.github.com>
2023-01-04 16:46:01 -0600

    1.5

--- pages/resume.js
+++ pages/resume.js
@@ -4,7 +4,9 @@ import styles from "./blog.module.css"
 export default function Resume(){
     return(
         <div className={styles.pdf}>
-            <iframe src="https://drive.google.com/file/d/1uaZmd11OyAyEROnBYo1YHMQjSLcEBTBj/preview" width="100%" height="100%" allowFullScreen = "true"></iframe>
+            <iframe src="https://drive.google.com/file/d/1uaZmd11OyAyEROnBYo1YHMQjSLcEBTBj/preview" width="100%" height="100%" allowFullScreen = "true">
+
+            </iframe>
         </div>
     )
 }

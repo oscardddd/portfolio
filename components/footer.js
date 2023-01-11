05ff7822a8467981bc2a56fc627b959fd3091c32
oscarddddddd <68830568+oscardddd@users.noreply.github.com>
2023-01-11 11:40:40 -0600

    mobile

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+import styles from "./header.module.css"
+
+export default function Footer(){
+    return(
+        <div className={styles.footer}>Proudly designed and created by Qianli Dong</div>
+    )
+}

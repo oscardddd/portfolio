f13928763bf3b17ab52b3564ca446b1ab965647a
oscarddddddd <68830568+oscardddd@users.noreply.github.com>
2023-05-06 14:43:48 -0500

    resume updated

--- pages/blog.js
+++ pages/blog.js
@@ -6,9 +6,9 @@ export default function Blog(){
         <div>
             <Header/>
             I am literally turning my goblin mode on rn ~
-            <h2 className={styles.text}> 
-            又该如何面对这碌碌无为的人生 
-            </h2>
+            {/* <h2 className={styles.text}> 
+           
+            </h2> */}
             
             
 

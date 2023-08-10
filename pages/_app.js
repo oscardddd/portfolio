061f9e6575bba5d50b665369131acf5ddba8df7e
oscarddddddd <qianlidong0@gmail.com>
2023-08-10 17:25:36 +0800

    favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "../global.css"
+// import Layout from "./_layout"
+import Head from "next/head"
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return(
+    <div>
+        <Head>
+            <title>Qianli's site</title>
+            <link rel='icon' type="image/x-icon" href="/images/minion.ico"/>
+        </Head>
+        <Component {...pageProps} />
+    </div>
+    )
+   
 }

09ad51794f001ca30b62b4c9561c4c9f062157ae
oscarddddddd <qianlidong0@gmail.com>
2024-04-17 00:14:21 -0500

    resume update

--- pages/resume.js
+++ pages/resume.js
@@ -1,12 +1,14 @@
-import styles from "./blog.module.css"
+import styles from "./blog.module.css";
 
-
-export default function Resume(){
-    return(
-        
-        <div className={styles.pdf}>
-            <iframe src="https://drive.google.com/file/d/1ZRUxQXA59W341RGzWyR3ourUNSTA7fWA/preview" width="100%" height="960" allow="autoplay"></iframe>
-                
-        </div>
-    )
+export default function Resume() {
+  return (
+    <div className={styles.pdf}>
+      <iframe
+        src="https://drive.google.com/file/d/1YpdrJY1r9NcY4tnkDbElfHzKy5DVcmrj/preview"
+        width="100%"
+        height="960"
+        allow="autoplay"
+      ></iframe>
+    </div>
+  );
 }
